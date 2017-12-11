f5262ed9a339d2acbc275afc4aa6f02e6ba3e202
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-11 13:36:29 +0100

    added test cases to test_route_utils

--- backend/test/test_route_util.js
+++ backend/test/test_route_util.js
@@ -10,6 +10,18 @@ describe('Array', function() {
       assert(RouteUtil.toArray(1) instanceof Array);
       assert(RouteUtil.toArray('') instanceof Array);
       assert(RouteUtil.toArray([]) instanceof Array);
+
+    });
+
+    it('should never nest Arrays', () => {
+      let testSubject = RouteUtil.toArray([0]);
+      assert.equal(testSubject[0], 0);
+      testSubject = RouteUtil.toArray(0);
+      assert.equal(testSubject[0], 0);
+      testSubject = RouteUtil.toArray(['test']);
+      assert.equal(testSubject[0], 'test');
+      testSubject = RouteUtil.toArray('test');
+      assert.equal(testSubject[0], 'test');
     });
   });
 });
