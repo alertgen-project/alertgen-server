2cd7c2bcd24be6c285f198cbb773dc1ea5cb49ca
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-19 19:37:00 +0100

    edited startscript

--- docker/start_mongo_admin.sh
+++ docker/start_mongo_admin.sh
@@ -1,9 +1,1 @@
-docker run -it --rm \
-    --name admin_mongo_alertgen \
-    -p 1015:1234 \
-    -e DB_HOST="http://allergy-check.informatik.hs-augsburg.de" \
-    -e DB_PORT="1010" \
-    -e CONN_NAME="alertgen" \
-    -e DB_USERNAME="admin" \
-    -e DB_PASSWORD="halloMongo;120" \
-    admin_mongo
+docker run --name admin_mongo_alertgen -p 1005:1234 -d admin_mongo
