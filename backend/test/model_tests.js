990fb02c37d41e98160158f74415ab6611b732b2
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-20 18:43:07 +0100

    adjusted modeltests

--- backend/test/model_tests.js
+++ backend/test/model_tests.js
@@ -2,24 +2,25 @@
 process.env.NODE_ENV = 'test';
 
 require('chai').should();
-const {getIngredientsModel} = require('../models/ingredient_model');
+const IngredientsModel = require('../models/ingredient_model');
 const {connectionFactory} = require('../models/connection_factory');
 
 describe('Ingredient Model Tests', () => {
 
   it('Should insert test-object, find it, update it and remove it',
       async () => {
-        const model = await getIngredientsModel();
-        (await model.insert({name: 'test'})).name.should.be.equal(
+        (await IngredientsModel.insert({name: 'test'})).name.should.be.equal(
             'test');
-        (await model.findOneIngredient({name: 'test'})).name.should.be.equal(
+        (await IngredientsModel.findOne({name: 'test'})).name.should.be.equal(
             'test');
-        (await model.removeOne({name: 'test'})).name.should.be.equal(
+        (await IngredientsModel.findByName('test'))[0].name.should.be.equal(
+            'test');
+        (await IngredientsModel.removeOne({name: 'test'})).name.should.be.equal(
             'test');
       });
 
   it('Should not find sugar and return false', async () => {
-    const model = await getIngredientsModel();
+    const model = await IngredientsModel.getIngredientsModel();
     const sugar = await model.updateIngredientAllergenConfirmation('sugar',
         'gluten',
         'contains_neg');
