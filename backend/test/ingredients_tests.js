49157ae8f8fafa75bc6332b95a98466deedd851b
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-25 12:48:55 +0100

    adjusted ingredients_test

--- backend/test/ingredients_tests.js
+++ backend/test/ingredients_tests.js
@@ -12,15 +12,14 @@ describe('ingredients', () => {
   describe(
       '/GET /ingredients?ingredients=DelicousPancakeDough&allergens=gluten',
       () => {
-        it('it should GET all the ingredients', (done) => {
+        it('it should GET the requested ingredients', (done) => {
           chai.request(server).
               get('/ingredients').
               query({ingredients: 'DelicousPancakeDough', allergens: 'gluten'}).
               end((err, res) => {
                 res.should.be.a.json;
                 res.should.have.status(200);
-                res.body[0].DelicousPancakeDough.gluten.containing.should.equal(
-                    true);
+                res.body[0].DelicousPancakeDough.gluten.containing.should.be.true;
                 done();
               });
         });
@@ -28,7 +27,7 @@ describe('ingredients', () => {
   describe(
       '/GET /ingredients?ingredients=DelicousPancakeDough&ingredients=DelicousPickle&allergens=gluten',
       () => {
-        it('it should GET all the ingredients', (done) => {
+        it('it should GET the requested ingredients', (done) => {
           chai.request(server).
               get('/ingredients').
               query({
@@ -38,10 +37,31 @@ describe('ingredients', () => {
               end((err, res) => {
                 res.should.be.a.json;
                 res.should.have.status(200);
-                res.body[0].DelicousPancakeDough.gluten.containing.should.equal(
-                    true);
-                res.body[1].DelicousPickle.gluten.containing.should.equal(
-                    false);
+                res.body[0].DelicousPancakeDough.gluten.containing.should.be.true;
+                res.body[1].DelicousPickle.gluten.containing.should.be.false;
+                done();
+              });
+        });
+      });
+  describe(
+      '/GET /ingredients?ingredients=sushimi&allergens=molluscs&allergens=gluten',
+      () => {
+        it('it should GET the requested ingredients', (done) => {
+          chai.request(server).
+              get('/ingredients').
+              query({
+                ingredients: ['sushimi'],
+                allergens: ['molluscs', 'gluten'],
+              }).
+              end((err, res) => {
+                res.should.be.a.json;
+                res.should.have.status(200);
+                res.body[0].sushimi.molluscs.containing.should.be.true;
+                res.body[0].sushimi.molluscs.contains_percent.should.equal(
+                    0.7);
+                res.body[0].sushimi.gluten.containing.should.be.false;
+                res.body[0].sushimi.gluten.contains_percent.should.equal(
+                    0);
                 done();
               });
         });
@@ -49,7 +69,7 @@ describe('ingredients', () => {
   describe(
       '/GET /ingredients?ingredients=DelicousPickle2&allergens=gluten&allergens=lupin',
       () => {
-        it('it should GET all the ingredients', (done) => {
+        it('it should GET the requested ingredients', (done) => {
           chai.request(server).
               get('/ingredients').
               query({
@@ -59,10 +79,8 @@ describe('ingredients', () => {
               end((err, res) => {
                 res.should.be.a.json;
                 res.should.have.status(200);
-                res.body[0].DelicousPickle2.gluten.containing.should.equal(
-                    false);
-                res.body[0].DelicousPickle2.lupin.containing.should.equal(
-                    false);
+                res.body[0].DelicousPickle2.gluten.containing.should.be.false;
+                res.body[0].DelicousPickle2.lupin.containing.should.be.false;
                 done();
               });
         });
