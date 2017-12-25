5eb71e58a454948e223e9b1497dff689d60a75c2
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-25 14:57:24 +0100

    added lock in help_us route, added parallel tests as integration tests for help_us

--- backend/routes/help_us.js
+++ backend/routes/help_us.js
@@ -11,6 +11,8 @@ const log = require('../logger/logger.js').getLog('help_us.js');
 const {UnexpectedError} = require('../errors/general_error_handling');
 const {Ingredient, increaseIngredientAllergen, decreaseIngredientAllergen, insert}
     = require('../models/ingredient_model.js');
+const AsyncLock = require('async-lock');
+const lock = new AsyncLock();
 
 async function postFeedback(ctx) {
   if (!ctx.query.ingredient || !ctx.query.allergen || !ctx.query.contains) {
@@ -25,42 +27,44 @@ async function postFeedback(ctx) {
   containsQueryParameter = containsQueryParameter === 'true';
   log.debug('Using Queryparameters:', ingredientQueryParameter,
       allergenQueryParameter);
-  let updateIsSuccessful = false;
-  try {
-    if (containsQueryParameter) {
-      updateIsSuccessful = await increaseIngredientAllergen(
-          ingredientQueryParameter, allergenQueryParameter);
-    } else {
-      updateIsSuccessful = await decreaseIngredientAllergen(
-          ingredientQueryParameter, allergenQueryParameter);
-    }
-  } catch (err) {
-    log.error(err);
-    console.error(err);
-    ctx.throw(new UnexpectedError());
-  }
-  if (!updateIsSuccessful) {
-    const ingredient = new Ingredient(
-        {name: ingredientQueryParameter});
-    const allergen = ingredient[allergenQueryParameter];
-    if (!allergen) {
-      ctx.throw(new AllergenNotFoundError(
-          {allergen: allergenQueryParameter}));
-    }
-    if (containsQueryParameter) {
-      allergen.contains_pos = 1;
-      allergen.contains_percent = 1;
-      allergen.contains = true;
-    } else {
-      allergen.contains_neg = 1;
-    }
+  await lock.acquire('ingredientUpdateRoute', async () => {
+    let updateIsSuccessful = false;
     try {
-      await insert(ingredient);
+      if (containsQueryParameter) {
+        updateIsSuccessful = await increaseIngredientAllergen(
+            ingredientQueryParameter, allergenQueryParameter);
+      } else {
+        updateIsSuccessful = await decreaseIngredientAllergen(
+            ingredientQueryParameter, allergenQueryParameter);
+      }
     } catch (err) {
       log.error(err);
       console.error(err);
       ctx.throw(new UnexpectedError());
     }
-  }
+    if (!updateIsSuccessful) {
+      const ingredient = new Ingredient(
+          {name: ingredientQueryParameter});
+      const allergen = ingredient[allergenQueryParameter];
+      if (!allergen) {
+        ctx.throw(new AllergenNotFoundError(
+            {allergen: allergenQueryParameter}));
+      }
+      if (containsQueryParameter) {
+        allergen.contains_pos = 1;
+        allergen.contains_percent = 1;
+        allergen.contains = true;
+      } else {
+        allergen.contains_neg = 1;
+      }
+      try {
+        await insert(ingredient);
+      } catch (err) {
+        log.error(err);
+        console.error(err);
+        ctx.throw(new UnexpectedError());
+      }
+    }
+  });
   ctx.body = {message: ingredientQueryParameter + ' updated'};
 }
--- backend/test/help_us_test.js
+++ backend/test/help_us_test.js
@@ -14,39 +14,96 @@ const {insert, findOneIngredientFuzzy, removeOne, Ingredient} = require(
 
 const testIngredientName = '4564456456test435345345';
 const testIngredient = new Ingredient({name: testIngredientName});
-;
 
 describe('ingredients', () => {
+
   describe(
       '/POST /helpus?ingredient=' + testIngredientName +
       '&allergen=gluten&contains=false',
       () => {
-        it('it insert and update a testingredient', (done) => {
-          chai.request(server).
+        it('it insert a ingredient manually and updates this inserted' +
+            ' testingredient', async () => {
+          await insert(testIngredient);
+          const res = await chai.request(server).
               post('/helpus').
               query({
                 ingredient: testIngredientName,
                 allergen: 'gluten',
                 contains: false,
-              }).
-              end((err, res) => {
-                res.should.be.a.json;
-                res.should.have.status(200);
-                findOneIngredientFuzzy(testIngredientName).
-                    then((ingredient) => {
-                      ingredient.name.should.be.equal(testIngredientName);
-                      ingredient.gluten.contains.should.be.false;
-                      done();
-                    }).
-                    catch(err => done(err));
               });
+          res.should.be.a.json;
+          res.should.have.status(200);
+          const ingredient = await findOneIngredientFuzzy(testIngredientName);
+          ingredient.name.should.be.equal(testIngredientName);
+          ingredient.gluten.contains.should.be.false;
+          ingredient.gluten.contains_neg.should.be.equal(1);
+          ingredient.gluten.contains_percent.should.be.equal(0);
         });
       });
+
+  describe(
+      '/POST /helpus?ingredient=' + testIngredientName +
+      '&allergen=gluten&contains=false',
+      () => {
+        it('it should ' +
+            'insert and update a testingredient automatically', async () => {
+          const res = await chai.request(server).
+              post('/helpus').
+              query({
+                ingredient: testIngredientName,
+                allergen: 'gluten',
+                contains: false,
+              });
+          res.should.be.a.json;
+          res.should.have.status(200);
+          const ingredient = await findOneIngredientFuzzy(testIngredientName);
+          ingredient.name.should.be.equal(testIngredientName);
+          ingredient.gluten.contains.should.be.false;
+          ingredient.gluten.contains_neg.should.be.equal(1);
+          ingredient.gluten.contains_percent.should.be.equal(0);
+        });
+      });
+
+  describe(
+      '/POST /helpus?ingredient=' + testIngredientName +
+      '&allergen=gluten&contains=true x10 times and false x7 times',
+      () => {
+        it('tests parallel /helpus-calls', async () => {
+          await Promise.all(
+              getHelpusRequests(10, testIngredientName, 'gluten', true));
+          let ingredient = await findOneIngredientFuzzy(testIngredientName);
+          ingredient.name.should.be.equal(testIngredientName);
+          ingredient.gluten.contains.should.be.true;
+          ingredient.gluten.contains_neg.should.be.equal(0);
+          ingredient.gluten.contains_pos.should.be.equal(10);
+          ingredient.gluten.contains_percent.should.be.equal(1);
+          await Promise.all(
+              getHelpusRequests(7, testIngredientName, 'gluten', false));
+          ingredient = await findOneIngredientFuzzy(testIngredientName);
+          ingredient.gluten.contains.should.be.true;
+          ingredient.gluten.contains_neg.should.be.equal(7);
+          ingredient.gluten.contains_pos.should.be.equal(10);
+          ingredient.gluten.contains_percent.should.be.equal(10 / 17);
+        });
+      });
+
+  function getHelpusRequests(count, name, allergen, contains) {
+    const requests = [];
+    for (let i = 0; i < count; i++) {
+      requests.push(chai.request(server).
+          post('/helpus').
+          query({
+            ingredient: name,
+            allergen: allergen,
+            contains: contains,
+          }));
+    }
+    return requests;
+  }
 });
 
 beforeEach(async () => {
   // make sure created objects are not in the database anymore
-  await insert(testIngredient);
 });
 
 afterEach(async () => {
