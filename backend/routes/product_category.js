7b814f0190c5f6b13828cbbafb464f405601df05
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-28 20:09:14 +0100

    added productCategoryRoute

--- backend/routes/product_category.js
+++ backend/routes/product_category.js
@@ -5,17 +5,45 @@ module.exports = {
 };
 
 const RouteUtil = require('./route_util.js');
-const ProductCategoryErrors = require('../errors/product_category_errors.js');
+const {
+  ProductCategoryWrongParameterError,
+  ProductCategoryNotFoundError,
+} = require('../errors/product_category_errors.js');
 const log = require('../logger/logger.js').getLog('product_category.js');
+const {findProductsOfCategory} = require('../models/product_model.js');
+const {findOneIngredientFuzzy} = require('../models/ingredient_model.js');
+const {AllergenNotFoundError} = require('../errors/ingredients_errors.js');
 
-async function retrieveProductsWithoutAllergens(ctx, next) {
+async function retrieveProductsWithoutAllergens(ctx) {
   if (!ctx.query.productCategory || !ctx.query.allergens) {
-    ctx.throw(new ProductCategoryErrors.ProductCategoryWrongParameterError());
+    ctx.throw(new ProductCategoryWrongParameterError());
   }
-  const productCategory = ctx.query.productCategory;
-  const allergens = RouteUtil.toArray(ctx.query.allergens);
-  log.debug('Using Queryparameters:', productCategory, allergens);
-  // request data from db
-  // form response
-  return ctx.body = true;
+  const productCategoryQueryParameter = ctx.query.productCategory;
+  const allergensQueryParameter = RouteUtil.toArray(ctx.query.allergens);
+  log.debug('Using Queryparameters:', productCategoryQueryParameter,
+      allergensQueryParameter);
+  const productsWithoutAllergens = [];
+  const products = await findProductsOfCategory(productCategoryQueryParameter);
+  if(!products ||products.length == 0){
+    ctx.throw(new ProductCategoryNotFoundError({category: productCategoryQueryParameter}));
+  }
+  for (let product of products) {
+    let containsAllergen = false;
+    for (let ingredientName of product.ingredients) {
+      const ingredientDocument = await findOneIngredientFuzzy(ingredientName);
+      for (let allergen of allergensQueryParameter) {
+        if (!ingredientDocument[allergen]) {
+          ctx.throw(new AllergenNotFoundError({allergen}));
+        }
+        if (ingredientDocument[allergen].contains) {
+          containsAllergen = true;
+        }
+      }
+    }
+    if (!containsAllergen) {
+      productsWithoutAllergens.push(
+          {productName: product.name, barcode: product.barcode});
+    }
+  }
+  return ctx.body = productsWithoutAllergens;
 }
