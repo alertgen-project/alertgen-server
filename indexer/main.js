e3e5581979bc88e445134331e164c1f865a2d9f1
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-28 15:12:36 +0100

    added async wrapper to main.js

--- indexer/main.js
+++ indexer/main.js
@@ -5,31 +5,24 @@ const ProductModel = require('../backend/models/product_model.js');
 const fs = require('fs');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
-
-const modelsToIndex = config.get('toIndex');
 const log = require('../backend/logger/logger.js').
     getLog('ingredient_model.js');
-const {connectionFactory} = require('../backend/models/connection_factory');
 
-let indexed = 0;
-
-modelsToIndex.forEach(async (model) => {
-  index(model).then(async () => {
-    indexed++;
-    if (indexed === modelsToIndex.length) {
-      await connectionFactory.closeConnection();
-    }
-  }).catch((err) => {
-    console.err(err);
-  });
-});
+// "main-function"
+(async() => {
+  const modelsToIndex = config.get('toIndex');
+  let indexed = 0;
+  for (let model of modelsToIndex){
+      await index(model);
+  }
+})();
 
 async function index(modelToIndex) {
   const documents = JSON.parse(
       (await readFile('./data/' + modelToIndex + '.json')).toString(
           'ascii'))[modelToIndex];
   if (modelToIndex === 'ingredients') {
-
+    await indexIngredients(documents);
   }
   if (modelToIndex === 'products') {
     await indexProducts(documents);
@@ -39,11 +32,10 @@ async function index(modelToIndex) {
 async function indexProducts(documents){
   for (let document of documents) {
     try {
-      return await ProductModel.insert(document);
+      await ProductModel.insert(document);
     } catch (err) {
       log.error(err);
       log.info('could not index', document.name);
-      return;
     }
   }
 }
