34f2e767740ba5a95f0bee5a5981e5172c5c5b79
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-30 12:38:07 +0100

    refactored indexer to fire all requests in parallel

--- indexer/main.js
+++ indexer/main.js
@@ -10,10 +10,10 @@ const log = require('../backend/logger/logger.js').
 const {connectionFactory} = require('../backend/models/connection_factory');
 
 // "main-function"
-(async() => {
+(async () => {
   const modelsToIndex = config.get('toIndex');
-  for (let model of modelsToIndex){
-      await index(model);
+  for (let model of modelsToIndex) {
+    await index(model);
   }
   await connectionFactory.closeConnection();
 })();
@@ -22,34 +22,42 @@ async function index(modelToIndex) {
   const documents = JSON.parse(
       (await readFile('./data/' + modelToIndex + '.json')).toString(
           'ascii'))[modelToIndex];
+  let pendingDocs;
   if (modelToIndex === 'ingredients') {
-    await indexIngredients(documents);
+    pendingDocs = await startIndexing(documents, IngredientsModel);
+    console.log(pendingDocs);
   }
   if (modelToIndex === 'products') {
-    await indexProducts(documents);
+    pendingDocs = await startIndexing(documents, ProductModel);
+    console.log(pendingDocs);
+  }
+  await waitForIndexing(pendingDocs);
+  log.info('finished indexing: ' + modelToIndex);
+}
+
+async function startIndexing(documents, model) {
+  const documentsWithRequests = [];
+  for (let document of documents) {
+    documentsWithRequests.push(
+        {name: document.name, promise: tryToInsert(document, model)});
+  }
+  return documentsWithRequests;
+}
+
+async function tryToInsert(document, model) {
+  try {
+    await model.insert(document)
+    log.info('indexed:', document.name);
+    return true;
+  } catch (err) {
+    log.error(err);
+    log.info('could not index:', document.name);
+    return null;
   }
 }
 
-async function indexProducts(documents){
+async function waitForIndexing(documents) {
   for (let document of documents) {
-    try {
-      await ProductModel.insert(document);
-      log.info('indexed: ' + document.name);
-    } catch (err) {
-      log.error(err);
-      log.info('could not index', document.name);
-    }
-  }
-}
-
-async function indexIngredients(documents){
-  for (let document of documents) {
-    try {
-      await IngredientsModel.insert(document);
-      log.info('indexed: ' + document.name);
-    } catch (err) {
-      log.error(err);
-      log.info('could not index', document.name);
-    }
+    await document.promise;
   }
 }
