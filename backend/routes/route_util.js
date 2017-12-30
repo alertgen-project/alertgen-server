c163f0c41e5a6f31d4babeaa78c5d1278e47a468
David Yesil <David.Yesil@hs-augsburg.de>
2017-12-30 17:06:10 +0100

    updated docstring of toArray

--- backend/routes/route_util.js
+++ backend/routes/route_util.js
@@ -4,12 +4,14 @@ module.exports = {
   toArray,
 };
 
+/**
+ * Used to make sure that the passed object uses an array as its container.
+   Creates an Array out of the referenced object if the passed reference
+   doesn't point to an array. Returns the array if an array has been passed.
+ * @param {object} object
+ * @returns {*}
+ */
 function toArray(object) {
-  /*
-    Used to make sure that the passed object uses an array as its container.
-    Creates an Array out of the referenced object if the passed reference
-    doesn't point to an array. Returns the array if an array has been passed.
-   */
   if (Array.isArray(object)) {
     return object;
   } else {
