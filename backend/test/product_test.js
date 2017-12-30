ca737e3d43e2adbee0916440e24b6fad2aa3cbd1
Andreas Kottre <andreas.kottre@hs-augsburg.de>
2017-12-30 17:41:02 +0100

    added tests for routes and edited error codes when sth not found to 404

--- backend/test/product_test.js
+++ backend/test/product_test.js
@@ -3,6 +3,7 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
+chai.should();
 const chaiHttp = require('chai-http');
 const chaiAsPromised = require('chai-as-promised');
 const server = require('../server.js');
@@ -14,6 +15,8 @@ describe('product route tests', () => {
 
   const testProductBarcode = '123123113';
   const testProductName = 'salami';
+  const invalidAllergen = 'lactose';
+  const wrongBarcode = '123123123123';
 
   it('should request product by name and say if contains requested allergens', async () => {
     const res = await chai.request(server).
@@ -40,6 +43,40 @@ describe('product route tests', () => {
     console.log(res.body);
     res.should.have.status(200);
   });
+
+  it('should throw ProductWrongParameterError because one parameter has been set wrong', async () => {
+    await chai.request(server).
+        get('/product').
+        query({
+          productCategory: testProductBarcode,
+          allergens: ['gluten', 'soy'],
+        }).catch((e) => {
+          e.should.have.status(400);
+        });
+  });
+
+  it('should throw ProductNotFoundError because the requested product cannot be found in database', async () => {
+    await chai.request(server).
+        get('/product').
+        query({
+          product: wrongBarcode,
+          allergens: ['gluten'],
+        }).catch((e) => {
+          e.should.have.status(404);
+        });
+  });
+
+  it('should throw InvalidAllergen because one of the requested allergens has been set wrong', async () => {
+    await chai.request(server).
+        get('/product').
+        query({
+          product: testProductBarcode,
+          allergens: [invalidAllergen],
+        }).catch((e) => {
+          e.should.have.status(400);
+        });
+  });
+
 });
 
 after(() => {
