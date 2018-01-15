aac3032985b6b3c605d8847f0b2bf53154df8aa7
David Yesil <David.Yesil@hs-augsburg.de>
2018-01-15 20:10:39 +0100

    added docstring

--- backend/test/test_util.js
+++ backend/test/test_util.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Generates a random number in the passed interval.
+ * @param {number} min smallest random number which can be generated
+ * @param {number} max biggest random number which can be generated
+ * @returns {number} the generated random number
+ */
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
